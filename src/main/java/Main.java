695ca18cd3ac2bc648b8efe5393a4ff6dc9678e9
Anna Paliy <annpaliy2015@gmail.com>
2024-11-11 10:55:26 +0300

    code reformated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-BonusService bonusService = new BonusService();
+        BonusService bonusService = new BonusService();
 
-long bonus =  bonusService.calculate(5_000, true);
-System.out.println(bonus);
+        long bonus = bonusService.calculate(5_000, true);
+        System.out.println(bonus);
     }
 }
